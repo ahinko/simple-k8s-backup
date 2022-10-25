5f306d543324524394caea5742866f724259ce77
Peter Ahinko <peter@ahinko.se>
2022-10-25 22:55:16 +0200

    date requires capital D when running on Alpine

--- sleep.sh
+++ sleep.sh
@@ -5,7 +5,7 @@ max_seconds="${2:-3600}"
 seconds="$(shuf -i "${min_seconds}"-"${max_seconds}" -n 1)"
 
 # current time - seconds
-start=$(date -d "$seconds seconds")
+start=$(date -D "$seconds seconds")
 echo "Will sleep for $seconds seconds. Backup will start at approximately $start"
 
 sleep $seconds
