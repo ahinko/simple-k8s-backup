9234d8fda1d5075d1174aed0149713e44741c970
Peter Ahinko <peter@ahinko.se>
2023-08-03 23:19:17 +0200

    Fix estimated time in sleep script output

--- sleep.sh
+++ sleep.sh
@@ -5,7 +5,7 @@ max_seconds="${2:-3600}"
 seconds="$(shuf -i "${min_seconds}"-"${max_seconds}" -n 1)"
 
 # current time - seconds
-start=$(date -D "$seconds seconds")
+start=$(date -d "@$(($(date +%s) + $seconds))")
 echo "Will sleep for $seconds seconds. Backup will start at approximately $start"
 
 sleep $seconds
